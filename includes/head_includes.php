d56495d820671b0ffac94f5f458cd1425602bff1
Willem Ellis <willem.ellis@gmail.com>
2013-11-24 14:27:54 -0500

    point to local leaflet stylesheets

--- includes/head_includes.php
+++ includes/head_includes.php
@@ -2,9 +2,9 @@
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
 
-        <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.6/leaflet.css" />
+        <link rel="stylesheet" href="./js/lib/leaflet/dist/leaflet.css" />
         <!--[if lte IE 8]>
-            <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.6/leaflet.ie.css" />
+            <link rel="stylesheet" href="./js/lib/leaflet/dist/leaflet.ie.css" />
         <![endif]-->
 
         <link rel="stylesheet" type="text/css" href="css/custom.css">
